689ddc0c91b47b5a94df050b00a6d88c7a585805
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2019-10-12 01:57:06 +0900

    openapiを出力可能にしてみる

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { BooksService } from './books.service';
 import Book from './book';
+import { ApiResponse, ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
+
   @Get()
+  @ApiOkResponse({ description: 'OK' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   findAll(): Promise<Book[]> {
     return this.booksService.findAll();
   }
